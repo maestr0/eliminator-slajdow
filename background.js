45b43d88e21de6aa27b4647afe94655bf67882a3
maestr0 <raszewski@gmail.com>
2012-12-25 18:39:09 +0000

    1.1.5 dodano pasek przewijania

--- background.js
+++ background.js
@@ -1,31 +1,9 @@
-var lastTabId = 0;
-
-
-//chrome.tabs.onSelectionChanged.addListener(checkForValidUrl);
-
-/*
-	function(tabId) {
-	lastTabId = tabId;
-	var icon = 'icon_48.jpg';
-	chrome.pageAction.setIcon({
-		path: icon,
-		tabId: tabId
-	});
-});
-
-
-
-chrome.tabs.getSelected(null, function(tab) {
-	lastTabId = tab.id;
-	chrome.pageAction.show(lastTabId);
-});
-
-*/
+// Listen for any changes to the URL of any tab.
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
 
 // Called when the url of a tab changes.
-
 function checkForValidUrl(tabId, changeInfo, tab) {
-	if (canRunOnCurrentUrl(tab.url) && localStorage['showIcon']!=="off") {
+	if (canRunOnCurrentUrl(tab.url)===true) {
 
 		var icon = 'icon_48.jpg';
 		chrome.pageAction.setIcon({
@@ -35,44 +13,49 @@ function checkForValidUrl(tabId, changeInfo, tab) {
 
 		chrome.pageAction.show(tabId);
 
+	} else if (canRunOnCurrentUrl(tab.url) === -1) {
+		var icon = 'icon_48_off.jpg';
+		chrome.pageAction.setIcon({
+			path: icon,
+			tabId: tabId
+		});
+
+		chrome.pageAction.show(tabId);
 	} else {
 		chrome.pageAction.hide(tabId);
 	}
 };
 
-// Listen for any changes to the URL of any tab.
-chrome.tabs.onUpdated.addListener(checkForValidUrl);
+// Listen for the content script to send a message to the background page.
+chrome.extension.onRequest.addListener(onRequest);
 
 function onRequest(request, sender, sendResponse) {
-
 	if (location.hostname == sender.id && request.urlName != undefined) {
 		sendResponse({
-			"canRunOnCurrentUrl": canRunOnCurrentUrl(request.urlName)
+			"canRunOnCurrentUrl": canRunOnCurrentUrl(request.urlName),
+			"scrollableImageContainer":localStorage['scrollableImageContainer']
 		});
 	}
 };
 // Return nothing to let the connection be cleaned up.
 
 function canRunOnCurrentUrl(hostname) {
-	if(localStorage['extensionStatus']==="off"){
-		return false;
-	}
-
 	var canRunHere = false;
 	var allowedDomains = JSON.parse(localStorage['allowedDomains']);
-	$.each(allowedDomains, function(allowedHost,enabled) {
-		if (enabled && hostname.indexOf(allowedHost) != -1) {
-			console.log('Rozszerzenie "Wszystkie zdjęcia na raz. GALERIA gazeta.pl" aktywne na: ' + allowedHost);
-			canRunHere = true;
-			return false;
+	$.each(allowedDomains, function(allowedHost, enabled) {
+		if (hostname.indexOf(allowedHost) != -1) {
+			if (enabled) {
+				console.log('Eliminator Slajdow aktywny na: ' + allowedHost);
+				canRunHere = true;
+			} else {
+				console.log('Eliminator Slajdow wyłączony na: ' + allowedHost);
+				canRunHere = -1; // flag indicating that the extension is disabled on the current url
+			}
+			return false; // brak the loop
 		};
 	});
 	return canRunHere;
 }
-
-// Listen for the content script to send a message to the background page.
-chrome.extension.onRequest.addListener(onRequest);
-
 
 function onInstall() {
 	console.log("Zainstalowano rozszerzenie 'Wszystkie zdjecia na raz. GALERIA gazeta.pl'");
@@ -103,15 +86,15 @@ if (currVersion != prevVersion) {
 }
 
 function updateAllowedDomainList() {
-	var standardAllowedDomains = new Array("gazeta.pl", "tokfm.pl", "gazetapraca.pl", "moto.pl", "plotek.pl", "deser.pl", "www.sport.pl", "wyborcza.pl", "gazetadom.pl", "www.logo24.pl", "wyborcza.biz", "lula.pl", "tuba.pl", "www.edziecko.pl", "czterykaty.pl", "www.alert24.pl", "www.kotek.pl", "polygamia.pl", "www.popcorner.pl", "www.wysokieobcasy.pl", "www.e-ogrody.pl", "ladnydom.pl", "bryla.gazetadom.pl", "gazetapraca.pl", "www.metropraca.pl", "pracawbiurze.pl", "www.zczuba.pl", "www.ciacha.net", "wyborcza.pl", "namonciaku.pl", "sport.pl", "magazyn-kuchnia.pl" ,"swiatmotocykli.pl","domosfera.pl");
+	var standardAllowedDomains = new Array("gazeta.pl", "tokfm.pl", "gazetapraca.pl", "moto.pl", "plotek.pl", "deser.pl", "www.sport.pl", "wyborcza.pl", "gazetadom.pl", "www.logo24.pl", "wyborcza.biz", "lula.pl", "tuba.pl", "www.edziecko.pl", "czterykaty.pl", "www.alert24.pl", "www.kotek.pl", "polygamia.pl", "www.popcorner.pl", "www.wysokieobcasy.pl", "www.e-ogrody.pl", "ladnydom.pl", "bryla.gazetadom.pl", "gazetapraca.pl", "www.metropraca.pl", "pracawbiurze.pl", "www.zczuba.pl", "www.ciacha.net", "wyborcza.pl", "namonciaku.pl", "sport.pl", "magazyn-kuchnia.pl", "swiatmotocykli.pl", "domosfera.pl");
 	localStorage['standardAllowedDomains'] = JSON.stringify(standardAllowedDomains);
 
-	if(typeof localStorage['allowedDomains'] !== "undefined") {
-		var allowedDomains = JSON.parse(localStorage['allowedDomains']);	
-	}else {
+	if (typeof localStorage['allowedDomains'] !== "undefined") {
+		var allowedDomains = JSON.parse(localStorage['allowedDomains']);
+	} else {
 		var allowedDomains = {};
 	}
-	
+
 
 	$.each(standardAllowedDomains, function(index, host) {
 
--- contentscript.js
+++ contentscript.js
@@ -1,93 +1,99 @@
 (function($) {
 	var self = this;
 	this.imageContainer;
-	this.sectionToBeRemovedSelector = ".navigation div, .navigation span.page";
+	this.sectionToBeRemovedSelector = ".navigation div, .navigation span.page, #gal_navi_wrp";
 	this.navigationNextULRSelector = ".navigation .next:first";
 	this.navigationPageNumberSelector = ".navigation .page:first";
 	this.articleBodySelector = "#gazeta_article_body";
 	this.sectionToBeAttached = "#gazeta_article_image img,#gazeta_article_body"; // sekcja komentarza i obrazek
 	this.headerSectionSelector = ".navigation:first h1 span";
 	this.hasSlideNumbers = true;
+	this.scrollableImageContainer = false;
+
+	/* START HERE */
 
 	chrome.extension.sendRequest({
-		"urlName": window.location.hostname
+		"urlName": window.location.href,
 	}, function(response) {
-		if(response.canRunOnCurrentUrl === true) {
+		if (response.canRunOnCurrentUrl === true) {
+			self.scrollableImageContainer = (response.scrollableImageContainer !== "off");
 			eliminateSlides();
 		}
 	});
 
-	/*
-	RUN EXTENSION ON CURRENT URL
-*/
 	function eliminateSlides() {
 
-		if($("body#pagetype_photo").length > 0) {
-			console.log("jestesmy na stronie z galeria #pagetype_photo");
+		if ($("body#pagetype_photo").length > 0) {
+			console.log("jestesmy na stronie z galeria #pagetype_photo (1)");
 			$("#gazeta_article_miniatures").empty();
 			loadImagesOnPage();
-		} else if($("body#pagetype_art_blog").length > 0) {
+		} else if ($("body#pagetype_art_blog").length > 0) {
 			/*
 				http://www.plotek.pl/plotek/56,78649,13096942,Kaja_Paschalska,,1.html
 				http://www.plotek.pl/plotek/56,79592,12829011,Jako_dzieci_byli_gwiazdami_seriali__Co_dzis_robia.html
 			*/
 			self.sectionToBeAttached = "#gazeta_article_image img,#gazeta_article_body, div[id*='gazeta_article_image_']:not('#gazeta_article_image_overlay')";
-			console.log("jestesmy na stronie z galeria #pagetype_art_blog");
+			console.log("jestesmy na stronie z galeria #pagetype_art_blog (2)");
 			loadImagesOnPage();
-		} else if($("body#pagetype_art").length > 0) {
-			console.log("jestesmy na stronie z galeria #pagetype_art");
+		} else if ($("body#pagetype_art").length > 0) {
+			console.log("jestesmy na stronie z galeria #pagetype_art (3)");
 			loadImagesOnPage();
 
-		} else if($("div#art div#container_gal").length > 0) {
+		} else if ($("div#art div#container_gal").length > 0) {
 			/*
 			Regresja
 			http://gazetapraca.pl/gazetapraca/56,90443,12057502,10_najdziwniejszych_powodow__dla_ktorych_rzucamy_prace.html
 			*/
 
-			console.log("jestesmy na stronie z gazetapraca.pl");
+			console.log("jestesmy na stronie z gazetapraca.pl (4)");
 			self.articleBodySelector = "#art";
 			self.navigationPageNumberSelector = ".paging:first";
-			self.sectionToBeRemovedSelector = "div#gal_navi_wrp";
+			self.sectionToBeRemovedSelector = "div#gal_navi_wrp, #gal_navi_wrp";
 			self.navigationNextULRSelector = "#gal_btn_next a:first";
 			self.sectionToBeAttached = "div#container_gal";
 			loadImagesOnPage();
 
-		} else if($("div#article div#article_body").length > 0) {
+		} else if ($("div#article div#article_body").length > 0) {
 			/*
 			Regresja
 			http://wyborcza.pl/duzy_kadr/56,97904,12530404,Najlepsze_zdjecia_tygodnia.html
 			*/
-			console.log("jestesmy na stronie z galeria div#article div#article_body");
+			console.log("jestesmy na stronie z galeria div#article div#article_body (5)");
 			self.articleBodySelector = "#article_body";
 			self.navigationNextULRSelector = "#gal_btn_next a:first";
-			self.sectionToBeRemovedSelector = "div#article ul";
+			self.sectionToBeRemovedSelector = "#gal_navi_wrp"; // div#article ul, 
 			self.sectionToBeAttached = "div#container_gal";
 			self.navigationPageNumberSelector = "#gal_navi .paging";
 			loadImagesOnPage();
-		} else if($("div#k1 div#k1p div#gal_outer").length > 0) {
+		} else if ($("div#k1 div#k1p div#gal_outer").length > 0) {
 			/*
 			Regresja
 			http://wyborcza.pl/51,75248,12537285.html?i=0
 			*/
-			console.log("jestesmy na stronie z galeria bez typu ('div#k1 div#k1p div#gal_outer')");
+			console.log("jestesmy na stronie z galeria bez typu ('div#k1 div#k1p div#gal_outer') (6)");
 			self.articleBodySelector = "div#gal_outer .description";
 			self.navigationNextULRSelector = "li.btn_next a:first";
-			self.sectionToBeRemovedSelector = "div#article ul";
+			self.sectionToBeRemovedSelector = "div#article ul, #gal_navi_wrp";
 			self.sectionToBeAttached = "div#gal_picture, div.description, p.description";
 			self.navigationPageNumberSelector = "#gal_navi .paging";
 			$("div#gal_miniatures").empty();
 			self.hasSlideNumbers = false;
 			loadImagesOnPage();
 		} else {
-			console.log("Nic mi nie pasuje ;(", document.location.hostname);
+			console.log("Eliminator Slajdow: Tutaj nic nie mam do roboty ;(", document.location.hostname);
 		}
 
 		function loadImagesOnPage() {
-			$(self.articleBodySelector).after("<div style='float:left' class='imageContainer'></div>");
-			self.imageContainer = $(self.articleBodySelector).parent().find(".imageContainer");
 			var nextPageURL = $(self.navigationNextULRSelector).attr("href");
 			console.log("link do nastepnej storny", nextPageURL);
-			if(nextPageURL) {
+			if (nextPageURL) {
+				var imageContainerStyle = 'float:left';
+				if (scrollableImageContainer) {
+					imageContainerStyle = 'display: inline-block;margin: 0 -25px 0 0;padding: 0 8px 0 0;overflow:scroll;height:1000px;overflow-x:hidden;overflow-y:scroll;';
+				}
+
+				$(self.articleBodySelector).after("<div style='" + imageContainerStyle + "' class='imageContainer'></div>");
+				self.imageContainer = $(self.articleBodySelector).parent().find(".imageContainer");
 				$.get(nextPageURL, function(nextPage) {
 					findNextSlideURL(nextPage);
 				});
@@ -96,36 +102,29 @@
 
 		function findNextSlideURL(galleryPage) {
 			var articleSection = $(galleryPage).find(self.sectionToBeAttached);
-			if($(articleSection).length > 0) {
+			if ($(articleSection).length > 0) {
 				pageNumber = $(galleryPage).find(self.navigationPageNumberSelector).text().split("/");
 				console.log("numer strony", pageNumber);
 				nextPageURL = $(galleryPage).find(self.navigationNextULRSelector).attr("href");
 
-				if(pageNumber.length===2) {
-					var slideSeparator = $(self.imageContainer).append("<div style='float:left;width:100%;margin-bottom:10px' class='slideNumber_"
-					 + pageNumber + 
-					 "'><p style='font-size: 12px;background: grey;padding: 3px;padding-left: 10px;border-radius: 42px;height: 14px;color: white;'>Slajd "
-					  + pageNumber[0] + " z " + pageNumber[1] + "</p><p style='margin-top:1px;float: right;font-size: 9px;'>Eliminator Slajdów</p></div>");
-				}else if(!hasSlideNumbers){
-					var slideSeparator = $(self.imageContainer).append("<div style='float:left;width:100%;margin-bottom=10px' class='slideNumber_"
-					 + pageNumber + 
-					 "'><p style='font-size: 12px;background: grey;padding: 3px;padding-left: 10px;border-radius: 42px;height: 14px;color: white;'>Slajd"
-					   + "</p><p style='margin-top:1px;float: right;font-size: 9px;'>Eliminator Slajdów</p></div>");
-				}else{
-					var slideSeparator = $(self.imageContainer).append("<div style='float:left;width:100%;margin-bottom=10px' class='slideNumber_"
-					 + pageNumber + 
-					 "'><p style='font-size: 12px;background: grey;padding: 3px;padding-left: 10px;border-radius: 42px;height: 14px;color: white;'>Ostatni slajd"
-					   + "</p><p style='margin-top:1px;float: right;font-size: 9px;'>Eliminator Slajdów</p></div>");
+				if (pageNumber.length === 2) {
+					var pageNumberLabel = "Slajd " + pageNumber[0] + " z " + pageNumber[1];
+				} else if (!hasSlideNumbers) {
+					var pageNumberLabel = "Slajd";
+				} else {
+					var pageNumberLabel = "Ostatni slajd";
 				}
 
+				$(self.imageContainer).append("<div style='float:left;width:100%;margin-bottom:10px' class='slideNumber_" + pageNumber + "'><p style='font-size: 12px;background: grey;padding: 3px;padding-left: 10px;border-radius: 42px;height: 14px;color: white;'>" + pageNumberLabel + "</p><p style='margin-top:1px;float: right;font-size: 9px;'>Eliminator Slajdów</p></div>");
+
 				var desc = $(galleryPage).find(self.headerSectionSelector).html();
-				if(desc){
-                    $(self.imageContainer).append("<p style='padding:20px;font-size:20px;'>" + desc + "</p>");
+				if (desc) {
+					$(self.imageContainer).append("<p style='padding:20px;font-size:20px;'>" + desc + "</p>");
 				}
 				$(articleSection).find(self.sectionToBeRemovedSelector).empty();
 				$(self.imageContainer).append($(articleSection));
 
-				if((pageNumber.length === 2 && pageNumber[0] !== pageNumber[1]) || (!hasSlideNumbers && document.location.href.indexOf(nextPageURL) === -1)) {
+				if ((pageNumber.length === 2 && pageNumber[0] !== pageNumber[1]) || (!hasSlideNumbers && document.location.href.indexOf(nextPageURL) === -1)) {
 					console.log("link do nastepnej storny", nextPageURL);
 					$.get(nextPageURL, function(nextPage) {
 						findNextSlideURL(nextPage);
@@ -134,11 +133,9 @@
 				$(self.sectionToBeRemovedSelector).empty();
 			}
 			$(".imageContainer > div").css("float", "left").css("width", "100%");
+			if($(".imageContainer").width()> 950){
+				$(".imageContainer").width(950);
+			}
 		}
-
-
-
-
-
 	}
 })(jQuery);
