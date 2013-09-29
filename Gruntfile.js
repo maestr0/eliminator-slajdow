373becfdcaebeedad27f9afadc8b3599c690cc22
Pawel Raszewski <pawel.raszewski@teamaol.com>
2013-09-29 22:53:13 +0100

    experimental setup for gruntJS builds

---
+++ Gruntfile.js
@@ -0,0 +1,95 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        manifest: grunt.file.readJSON('manifest.json'),
+        concat: {
+            options: {
+                separator: ';'
+            },
+            popup: {
+                src: ['jquery-ui-1.8.16.custom.min.js', 'jquery.iphone-switch.js', 'popup.js'],
+                dest: 'package/js/popup.js'
+            }
+        },
+        compass: {
+            dist: {                   // Target
+                options: {              // Target options
+                    sassDir: 'scss',
+                    cssDir: 'package/css',
+                    environment: 'production'
+                }
+            },
+            dev: {                    // Another target
+                options: {
+                    sassDir: 'sass',
+                    cssDir: 'css',
+                    environment: 'production'
+                }
+            }        },
+        replace: {
+            dist: {
+                options: {
+                    variables: {
+                        'version': '<%=pkg.version%>'
+                    },
+                    prefix: '@@'
+                },
+                files: [
+                    {expand: true, flatten: true, src: ['manifest.json'], dest: 'package/'}
+                ]
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*!\n<%= manifest.name %> v<%=pkg.version %>\nAuthor: <%= pkg.author%>\nBuild: <%= grunt.template.today("dd-mm-yyyy hh:MM:ss") %> */\n'
+            },
+            dist: {
+                files: {
+                    'package/js/contentscript.min.js': ['package/js/contentscript.js'],
+                    'package/js/popup.min.js': ['package/js/popup.js'],
+                    'package/js/background.min.js': ['package/js/background.js']
+                }
+            }
+        },
+        jshint: {
+            files: ['contentscript.js', 'background.js', 'popup.js'],
+            options: {
+                // options here to override JSHint defaults
+                globals: {
+                    jQuery: true,
+                    console: true,
+                    module: true,
+                    document: true
+                }
+            }
+        },
+        copy: {
+            main: {
+                files: [
+                    {expand: true, src: ['css/**', 'background.html', 'popup.html'], dest: 'package/'},
+                    {expand: true, src: ['css/**', 'background.html', 'popup.html'], dest: 'package/'},
+                    {expand: true, src: ['icon*.jpg', 'iphone*.png', 'ajax-loader.gif', 'icon_facebook.gif'], dest: 'package/images'}
+                ]
+            }
+        },
+        watch: {
+            files: ['<%= jshint.files %>', 'scss/es.scss', '*.html'],
+            tasks: ['jshint', 'concat', 'uglify', 'compass']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-replace');
+
+    grunt.registerTask('test', ['jshint']);
+
+    grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'compass']);
+    grunt.registerTask('package', ['jshint', 'concat', 'uglify', 'compass', 'replace', 'copy']);
+
+};
