3638368f32ff22548bfb18133e882ae546afdd8f
Pawel Raszewski <raszewski@gmail.com>
2014-08-26 22:28:56 +0100

    3.1.24

--- firefox/data/eliminator-slajdow.jquery.widget.js
+++ firefox/data/eliminator-slajdow.jquery.widget.js
@@ -5,9 +5,9 @@
  *   Licencja: GPLv3
  *   Strona Domowa: http://eliminator-slajdow.herokuapp.info
  *
- *   CDN: http://cdn.jsdelivr.net/jquery.eliminator-slajdow/3.1.22/eliminator-slajdow.jquery.widget.js
+ *   CDN: http://cdn.jsdelivr.net/jquery.eliminator-slajdow/3.1.24/eliminator-slajdow.jquery.widget.js
  *
- *   Wersja: 3.1.22
+ *   Wersja: 3.1.24
  * */
 
 (function ($) {
