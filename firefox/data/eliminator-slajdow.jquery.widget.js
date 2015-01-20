1f14f97866ab2ebe0fc9605d5c621e4dd521c6b2
Pawel Raszewski <raszewski@gmail.com>
2015-01-20 13:25:44 +0100

    wydanie 3.1.40

--- firefox/data/eliminator-slajdow.jquery.widget.js
+++ firefox/data/eliminator-slajdow.jquery.widget.js
@@ -5,9 +5,9 @@
  *   Licencja: GPLv3
  *   Strona Domowa: http://eliminator-slajdow.herokuapp.com
  *
- *   CDN: http://cdn.jsdelivr.net/jquery.eliminator-slajdow/3.1.39/eliminator-slajdow.jquery.widget.js
+ *   CDN: http://cdn.jsdelivr.net/jquery.eliminator-slajdow/3.1.40/eliminator-slajdow.jquery.widget.js
  *
- *   Wersja: 3.1.39
+ *   Wersja: 3.1.40
  * */
 
 (function ($) {
@@ -20,7 +20,7 @@
             facebookUrl: "https://www.facebook.com/eliminator-slajdow?ref=chrome.extension",
             bugReportUrl: "http://eliminator-slajdow.herokuapp.com/?ref=chrome.extension",
             debug: false,
-            version: "3.1.39",
+            version: "3.1.40",
             customPages: {},
             trackingCallback: function (category, action) {
             }
