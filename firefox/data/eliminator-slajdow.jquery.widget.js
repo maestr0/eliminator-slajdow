6ccd83135f91e176a5e08467dab013cce3cbb1ab
Pawel Raszewski <raszewski@gmail.com>
2015-01-20 15:00:58 +0100

    poprawki dla wersji 3.1.40 dla firefoxa

--- firefox/data/eliminator-slajdow.jquery.widget.js
+++ firefox/data/eliminator-slajdow.jquery.widget.js
@@ -2214,7 +2214,6 @@
             }
         },
         _logger: function () {
-            console.log.apply(console, arguments);
             if (this.options.debug) {
                 $("#es_debug").val($("#es_debug").val() + "\n" + JSON.stringify(arguments)).animate({
                     scrollTop: 10000000
