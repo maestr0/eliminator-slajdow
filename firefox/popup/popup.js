755007d4da9b708f86428337575afe0f63cd7b5e
Pawel Raszewski <raszewski@gmail.com>
2016-12-20 23:53:45 -0500

    popup

--- firefox/popup/popup.js
+++ firefox/popup/popup.js
@@ -4,18 +4,23 @@ function getActiveTab() {
 
 $("#options").click(()=> {
     browser.runtime.openOptionsPage();
+    this.close();
 });
 
 $("#disable").click(()=> {
     browser.storage.local.get('status').then((res)=> {
         var newStatus = parseInt(res.status) * -1;
+        $("#disable").addClass("button-status" + newStatus);
+        $("#disable").removeClass("button-status" + (newStatus * -1));
         browser.runtime.sendMessage({"status": newStatus});
+        this.close()
     });
 });
 
 function init() {
     browser.storage.local.get('status').then((res)=> {
-        $("#disable").text("ES " + res.status)
+        $("#disable").addClass("button-status" + res.status)
+        $("#disable").removeClass("button-status" + (res.status * -1));
     });
 }
 
